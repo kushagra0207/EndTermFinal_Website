454c5e81a899d1552059972508730441e2b2ce48
Kushagra Gupta <kushagra.guptafzd02@gmail.com>
2023-05-12 16:25:13 +0530

    Final Commit To My Gla Project

---
+++ server/src/routes/testDataRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const { seedDB, getCollectionDocuments } = require('../config/db');
+const validate = require('../middleware/validate');
+
+router.post('/seed', async (req, res) => {
+  await seedDB();
+  res.status(200).send();
+});
+
+const entityValidation = {
+  params: {
+    entity: Joi.string().valid('users', 'profiles', 'tweets'),
+  },
+};
+
+router.get('/:entity', validate(entityValidation), async (req, res) => {
+  const { entity } = req.params;
+
+  const collections = mongoose.connection.collections;
+
+  const collection = collections[`${entity}`];
+
+  const results = await getCollectionDocuments(collection);
+
+  res.send(results);
+});
+
+module.exports = router;
+// We can also choose new Url For Development and host accordingly to wish
+// Kushagra Gupta GLA University Mathura
